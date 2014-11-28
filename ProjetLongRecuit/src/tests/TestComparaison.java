9630f0e4ec4b10724a1e7bc7f128433ccb760084
darkpudding <loucarapuce2685939@hotmail.com>
2014-11-28 16:08:54 +0100

    -TestComparaison entre l'ancien RecuitSimule et le nouveau RecuitSimuleParametrable avec les classes TemperatureLineairePalier et ConstanteKConstant, les resultats sont coherents. -Ajout de TemperatureLineairePalier.

--- ProjetLongRecuit/src/tests/TestComparaison.java
+++ ProjetLongRecuit/src/tests/TestComparaison.java
@@ -11,6 +11,9 @@ import solver.MutationConflitsAleatoire;
 import solver.RecuitSimule;
 import solver.RecuitSimuleLineaire;
 import solver.Traducteur;
+import solverSimuleParametrable.ConstanteKConstant;
+import solverSimuleParametrable.RecuitSimuleParametrable;
+import solverSimuleParametrable.TemperatureLineairePalier;
 
 public class TestComparaison {
 	
@@ -22,16 +25,19 @@ public static void main(String[] args) throws IOException {
 	MutationConflitsAleatoire mutation = new MutationConflitsAleatoire();
 	
 	Graphe graphe = Traducteur.traduire("data/dsjc250.5.col");
+	
 	int nbNoeuds = 250;
 	int nbCouleurs = 28;
 	double k = 1;
 	int M = 4;//* nbNoeuds * nbCouleurs;
 	double T0 = 0.35;
-	double maxSteps = Math.pow(10,5);
-	int seed = 7;
+	int maxSteps = (int) Math.pow(10,5);
+	int seed = 1;
 		
 	GrapheColorieParticule coloriage = new GrapheColorieParticule(Ep, mutation, Ec, nbCouleurs, 1, graphe, seed);
 	coloriage.initialiser();
+	
+	// RecuitSimule ancien
 	RecuitSimule recuit = new RecuitSimuleLineaire(k,T0,0,T0/maxSteps,M);						
 
     ThreadMXBean temp = ManagementFactory.getThreadMXBean( );  // recuperer temps cpu
@@ -42,7 +48,22 @@ public static void main(String[] args) throws IOException {
 	long endTime = System.nanoTime();
 	
 	System.out.println("seed = "+seed +".  Nombre de conflits : "+recuit.getMeilleureEnergie()+", Duree = "+(endTime-startTime)/1000000000+" s"+", Duree CPU = "+(endCpu-startCpu)/1000000000+" s");
+	
+	//RecuitSimuleParametrable
+	ConstanteKConstant Kparam = new ConstanteKConstant(k);
+	TemperatureLineairePalier Tparam = new TemperatureLineairePalier(T0,0,maxSteps,M);
+	RecuitSimuleParametrable recuitParam = new RecuitSimuleParametrable(Tparam,Kparam);						
 
+    ThreadMXBean temp1 = ManagementFactory.getThreadMXBean( );  // recuperer temps cpu
+	long startTime1 = System.nanoTime();
+	long startCpu1 = temp1.getCurrentThreadCpuTime();	
+	recuitParam.lancer(coloriage);
+	long endCpu1 = temp1.getCurrentThreadCpuTime();
+	long endTime1 = System.nanoTime();
+	
+	System.out.println("seed = "+seed +".  Nombre de conflits : "+recuit.getMeilleureEnergie()+", Duree = "+(endTime1-startTime1)/1000000000+" s"+", Duree CPU = "+(endCpu1-startCpu1)/1000000000+" s");
+	
+	
 	}
 
 }
