f4e3a82df0a1e62bc0779adb9568fb8cc8e3cb6a
FabienDufoulon <fabien.dufoulon@supelec.fr>
2015-01-29 14:24:38 +0100

    Quelques essais sur la Javadoc au niveau d'EnergieCinétique et EnergiePotentielle.

--- ProjetLongRecuit/src/solverCommun/EnergiePotentielle.java
+++ ProjetLongRecuit/src/solverCommun/EnergiePotentielle.java
@@ -2,7 +2,22 @@ package solverCommun;
 
 public abstract class EnergiePotentielle{
 	
+	/**
+	 * 
+	 * @param etat
+	 * Etat modifié par la mutation.
+	 * @return L'énergie potentielle de l'état modifié.
+	 */
 	abstract public double calculer(Etat etat);
+	
+	/**
+	 * 
+	 * @param etat
+	 * Etat modifié par la mutation.
+	 * @param mutation
+	 * Mutation affectant l'état en question.
+	 * @return La différence d'énergie potentielle sur l'état modifié par la mutation donnée.
+	 */
 	abstract public double calculerDeltaE(Etat etat, MutationElementaire mutation);
 	
 }
