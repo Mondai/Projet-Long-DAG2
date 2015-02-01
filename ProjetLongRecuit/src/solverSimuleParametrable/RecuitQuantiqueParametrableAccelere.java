484c6a25679872f1d7b26b6a2e5ebaaa5c08d0ad
FabienDufoulon <fabien.dufoulon@supelec.fr>
2015-02-01 22:12:29 +0100

    Mise en place de compteurs pour les mutations tentées et les mutations acceptées. Cela va nous permettre d'avoir une meilleure idée de l'impact des améliorations proposées par l'article quand elles seront implémentées, mais aussi de comparer plus fidèlement nos chiffres avec ceux de l'article et d'avoir une idée des endroits où l'algorithme prend le plus de temps

--- ProjetLongRecuit/src/solverSimuleParametrable/RecuitQuantiqueParametrableAccelere.java
+++ ProjetLongRecuit/src/solverSimuleParametrable/RecuitQuantiqueParametrableAccelere.java
@@ -10,7 +10,7 @@ import solverCommun.MutationElementaire;
 import solverCommun.Probleme;
 
 
-public class RecuitQuantiqueParametrable extends RecuitSimuleP { 				
+public class RecuitQuantiqueParametrableAccelere extends RecuitSimuleP { 				
 																		// creer vos propres Temperature, ConstanteK et trucs pour les graphes
 	public Temperature Gamma;
 	public ConstanteK K;
@@ -21,7 +21,7 @@ public class RecuitQuantiqueParametrable extends RecuitSimuleP {
 	public int palier;
 	// abstract void init(); 								// initialisation // mais de quoi ?
 
-	public RecuitQuantiqueParametrable(Temperature Gamma, ConstanteK K, int palier, double temperature) {
+	public RecuitQuantiqueParametrableAccelere(Temperature Gamma, ConstanteK K, int palier, double temperature) {
 		this.Gamma=Gamma;												// contructeur : on lui donne la facon de calculer l'energie, K et tout le blabla
 		this.K=K;												// en creant une classe dedie et reutilisable qui extends temperature
 		this.nbMaxIteration=this.Gamma.nbIteration;						// ainsi on combine le tout facilement
@@ -38,10 +38,6 @@ public class RecuitQuantiqueParametrable extends RecuitSimuleP {
 		 * (peut-être changer les classes Temperature à un nom plus neutre)
 		 * Enlever les variables de spin???		
 		*/
-		
-		int mutationsTentees = 0;
-		int mutationsAcceptees = 0;
-		
 		int nombreRepliques = probleme.etats.length;
 		
 		Etat etat = probleme.etats[0];
@@ -99,11 +95,9 @@ public class RecuitQuantiqueParametrable extends RecuitSimuleP {
 					System.out.println("]");*/
 					
 					MutationElementaire mutation = probleme.getMutationElementaire(etat);	// trouver une mutation possible
-					mutationsTentees++; //permet d'avoir une référence indépendante pour les améliorations de l'algorithme, mais aussi sur son temps
-					
 					double deltaEp = probleme.calculerDeltaEp(etat, mutation);	// calculer deltaEp si la mutation etait acceptee
 					double deltaEc = probleme.calculerDeltaEc(etat, previous, next, mutation);  // calculer deltaIEc si la mutation etait acceptee
-						
+
 					//différences du hamiltonien total
 					//multiplier deltaIEc par JGamma
 					double deltaE = deltaEp/nombreRepliques - deltaEc*Jr;
@@ -113,53 +107,42 @@ public class RecuitQuantiqueParametrable extends RecuitSimuleP {
 					if( deltaE <= 0 || deltaEp <= 0){
 						
 						// System.out.println("acceptee"); //TEST
-						mutationsAcceptees++;
 						probleme.modifElem(etat, mutation);				// faire la mutation
 						double EpActuelle = etat.Ep.calculer(etat);		// energie potentielle temporelle
 						if( EpActuelle < this.meilleureEnergie ){		// mettre a jour la meilleur energie
 							this.meilleureEnergie = EpActuelle;
 							// TEST
-							/*
 							System.out.print("etat "+p+" : ME = "+this.meilleureEnergie+" , G = "+this.Gamma.t+" , Jr = "+Jr+" .  Ep = [");
 							System.out.print(probleme.etats[0].Ep.calculer(probleme.etats[0]));
 							for(int w = 1 ; w < nombreRepliques ; w++){
 								System.out.print(","+probleme.etats[w].Ep.calculer(probleme.etats[w]));
 							}
-							System.out.println("]");*/
+							System.out.println("]");
 							// TEST
 							if (this.meilleureEnergie == 0){	// fin du programme
-								System.out.println("Mutations tentées : " + mutationsTentees);
-								System.out.println("Mutations acceptées : " + mutationsAcceptees);
 								return probleme;
 							}
 						}
 					} else {
 						proba = Math.exp(-deltaE / (this.K.k * this.temperature));	// calcul de la proba
 						//System.out.println("Proba : " + proba); //TEST
-						if (proba >= probleme.gen.nextDouble()) {	
-							mutationsAcceptees++;
+						if (proba >= probleme.gen.nextDouble()) {							
 							probleme.modifElem(etat, mutation);  		// accepter la mutation 
 						}
 					}
 				}
 				// TESTTEST
-				/*
 				System.out.print("etat "+p+" : ME = "+this.meilleureEnergie+" , G = "+this.Gamma.t+ " , Jr = "+Jr +" .  Ep = [");
 				System.out.print(probleme.etats[0].Ep.calculer(probleme.etats[0]));
 				for(int w = 1 ; w < nombreRepliques ; w++){
 					System.out.print(","+probleme.etats[w].Ep.calculer(probleme.etats[w]));
 				}
-				System.out.println("]");*/
+				System.out.println("]");
 				// TEST
 				//System.out.println("Fin " + i + " : etat "+etat.toString()); //TEST	
-				
-
 			}
 		}
 		
-		
-		System.out.println("Mutations tentées : " + mutationsTentees);
-		System.out.println("Mutations acceptées : " + mutationsAcceptees);
 		return probleme;
 	}
 }
