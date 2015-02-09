08c28afe8623b9756c72a5690f60bc14a0530e27
darkpudding <lou.salaun@supelec.fr>
2015-02-09 12:21:48 +0100

    Implementation de nextInt(int) dans HighQualityRandom, et ajout dans MutationConflitsAleatoire a la place de nextDouble().

--- ProjetLongRecuit/src/solver/HighQualityRandom.java
+++ ProjetLongRecuit/src/solver/HighQualityRandom.java
@@ -58,5 +58,20 @@ public class HighQualityRandom extends Random {
 	protected int next(int bits) {
 		return (int) (nextLong() >>> (64 - bits));
 	}
+	
+	public int nextInt(int n) {
+		if (n <= 0)
+			throw new IllegalArgumentException("n must be positive");
+
+		if ((n & -n) == n)  // i.e., n is a power of 2
+			return (int)((n * (long)next(31)) >> 31);
+
+		int bits, val;
+		do {
+			bits = next(31);
+			val = bits % n;
+		} while (bits - val + (n-1) < 0);
+		return val;
+	}
 
 }
