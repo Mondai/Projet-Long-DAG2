2612f9cfde70724795f934e87f48184f83bc6a4b
Mondai <stephane.lundy@supelec.fr>
2015-02-11 11:49:45 +0100

    La mire est normallement fonctionnelle

--- ProjetLongRecuit/src/algoDeLaMire/Mire.java
+++ ProjetLongRecuit/src/algoDeLaMire/Mire.java
@@ -1,6 +1,7 @@
 package algoDeLaMire;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import solver.ConflitsCinetiques;
 import solver.EnergieCinetiqueVide;
@@ -31,8 +32,9 @@ public class Mire {
 	double T;
 	double deltaT;
 	Graphe graphe;
-	int pointCentral ;  //c'est le nombre d'itération pour un recuit ( voir si fut en prendre plus ! )
+	int pointCentral ;  //c'est la somme des nombres d'itération pour 5 recuits
 	
+	//les variables suivantes dépendent du graphe
 	int nbNoeuds = 250;
 	int nbCouleurs = 28;
 	double k = 1;
@@ -40,15 +42,19 @@ public class Mire {
 	int P = 10;
 	int maxSteps = 1000;
 	int seed = 745267;
+	
 
 	public static void main(String[] args) throws IOException {
 		
+		// ici le main qui lance l'algo
+		
 		Mire mire = new Mire();
-		mire.init(10,0.35); 
+		mire.init(10,0.35); //paramètre initiaux à changer
+		mire.lancerMire();
 
 	}
 	
-	Mire() {} 
+	public  Mire() {} ;
 	
 	// la création de mire dépend actuellement des caractéristique de "data/dsjc250.5.col"
 	
@@ -56,8 +62,10 @@ public class Mire {
 		this.graphe = Traducteur.traduire("data/dsjc250.5.col");
 		this.G0=G0init;
 		this.T=Tinit; 
+		this.deltaT = Tinit*2/3; 
+		this.deltaG0 = G0init*2/3;
 		this.pointCentral = testerParam(G0,T);
-		//les deltas deplacements à changer aussi
+		
 		
 		}
 
@@ -73,8 +81,12 @@ public class Mire {
 		ConstanteKConstant Kparam = new ConstanteKConstant(k);
 		RecuitQuantiqueParametrableAccelere recuit = new RecuitQuantiqueParametrableAccelere(
 				Tparam, Kparam, M, t);
+		double somme = 0. ;
+		for (int i = 0 ; i < 5 ; i++) {
+			somme = somme + recuit.lancer(coloriage) ;
+		}
 		
-			return recuit.lancer(coloriage);
+			return (int) somme ;
 
 	}
 	
@@ -88,22 +100,42 @@ public class Mire {
 		int droit = testerParam(G0+deltaG0,T);
 		int bas = testerParam(G0,T-deltaT);
 		
-		/*
-		 * comparaison avec le point central puis
-		 * modification du point central
-		 * et eventuellement des deltas deplacements
-		 */
+		int[] tab = new int[] {this.pointCentral, haut, gauche, droit, bas} ;
+		Arrays.sort(tab);
+		
+		if (tab[0] == this.pointCentral ) {
+		this.deltaG0 = this.deltaG0 / 2 ;
+		this.deltaT = this.deltaT / 2 ;
+		}
+		else if (tab[0] == haut) {
+			this.pointCentral=haut;
+			this.T=T+deltaT;
+		} else if (tab[0] == bas) {
+			this.pointCentral=bas;
+			this.T=T-deltaT;
+			if (T-deltaT < 0) {
+				deltaT=0.9*T;
+			}
+		} else if (tab[0] == gauche) {
+			this.pointCentral=gauche;
+			this.G0=G0-deltaG0;
+			if (G0-deltaG0 < 0) {
+				deltaG0=0.9*G0;
+			}
+		} else if (tab[0] == droit) {
+			this.pointCentral=droit;
+			this.G0=G0+deltaG0;
+		} 
 				
 	}
 	
 	public void lancerMire () {
-		/* il reste à :
-		 * trouver un critère d'arret et donc modifier le true
-		 * par exemple : terminer en 10 étapes YOLO / stationnarité / delta < ?
-		 */
-		while (true) {
+
+		int compteur = 0 ;
+		while (compteur < 10) {  // changer la valeur max pour augmenter la durée de la mire
 			this.UneEtape();
 			System.out.println("G0 actuel "+this.G0+" et T actuel "+this.T);
+			compteur ++ ;
 		}
 	}
 	
