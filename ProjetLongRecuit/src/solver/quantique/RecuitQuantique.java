e4b79b0f26651888afe54ca0f0c1615def43e9d4
darkpudding <lou.salaun@supelec.fr>
2015-04-11 09:48:54 +0200

    test mutationsTentees

--- ProjetLongRecuit/src/solver/quantique/RecuitQuantique.java
+++ ProjetLongRecuit/src/solver/quantique/RecuitQuantique.java
@@ -141,7 +141,6 @@ public class RecuitQuantique implements IRecuit {
 			double DE = 0;
 			
 			for (Integer p : indiceEtats){	
-				mutationsTentees++;
 				
 				etat = probleme.etats[p];	
 				
@@ -163,7 +162,7 @@ public class RecuitQuantique implements IRecuit {
 				for (int j = 0; j < this.palier; j++){
 
 					MutationElementaire mutation = probleme.getMutationElementaire(etat);	// trouver une mutation possible
-					//mutationsTentees++; //permet d'avoir une référence indépendante pour les améliorations de l'algorithme, mais aussi sur son temps
+					mutationsTentees++; //permet d'avoir une référence indépendante pour les améliorations de l'algorithme, mais aussi sur son temps
 					
 					double deltaEp = probleme.calculerDeltaEp(etat, mutation);	// calculer deltaEp si la mutation etait acceptee
 					double deltaEc = probleme.calculerDeltaEc(etat, previous, next, mutation);  // calculer deltaIEc si la mutation etait acceptee
